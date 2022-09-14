6926a2d5966e6f7e792c598d8a27acc2771ce310
Charles Hall <charles@computer.surgery>
2022-09-14 15:39:44 -0700

    improve command line help messages

--- src/args.rs
+++ src/args.rs
@@ -3,17 +3,36 @@
 use clap::Parser;
 
 /// A task runner with DAG-based parallelism
+///
+/// * All task commands are executed with the working directory set to the
+///   location of the Engage file.
+///
+/// * Subcommands that require the Engage file can be executed from any
+///   directory so long as either the current directory or any of its ancestors
+///   contain the Engage file.
+///
+/// * Group and task dependencies must form a directed acyclic graph; Engage
+///   will enforce this. In other words, dependency cycles are not allowed.
+///
+/// * If a task fails, any subsequent tasks will not be executed and Engage will
+///   exit with the same value as the failed task.
+///
+/// * If no subcommand is supplied, all groups and tasks will be scheduled based
+///   on their dependencies and executed appropriately.
 #[derive(Parser)]
 #[clap(author, version, about)]
 pub struct Args {
     /// Available subcommands
     ///
-    /// If unspecified, all groups and tasks in the Engage file are run.
+    /// If `None`, all groups and tasks in the Engage file are run. This doc
+    /// comment does not appear in help messages.
     #[clap(subcommand)]
     pub subcmd: Option<Subcommand>,
 }
 
 /// Top-level subcommands
+///
+/// This doc comment does not appear in help messages.
 #[derive(clap::Subcommand)]
 pub enum Subcommand {
     /// Run a built-in command
@@ -25,6 +44,8 @@ pub enum Subcommand {
 }
 
 /// Built-in commands
+///
+/// This doc comment does not appear in help messages.
 #[derive(clap::Subcommand)]
 pub enum Builtin {
     /// Output Graphviz' `dot` representation of the task/group DAG and exit
@@ -34,14 +55,15 @@ pub enum Builtin {
 /// A specific group or task to run
 #[derive(clap::Args)]
 pub struct Just {
-    /// A specific group of tasks to run
+    /// The group to run
     ///
-    /// Does not run any of the dependencies of this group
+    /// Does not run any of this group's dependencies. Schedules tasks
+    /// according to their dependencies, as normal.
     pub group: String,
 
-    /// A specific task in the group to run, if any
+    /// The task in <GROUP> to run
     ///
     /// If specified, the dependencies of the chosen task will not be run, only
-    /// the chosen task itself.
+    /// the task itself.
     pub task: Option<String>,
 }
