gamesguru/engage

commit 64f24a6dcfcf11d3447628b3a9b0838d0b933bf3

use `naersk` instead of `buildRustPackage`

flake.nix
@@ -7,6 +7,11 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -15,46 +20,43 @@
     , flake-utils
 
     , fenix
+    , naersk
     }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
-      rust = fenix.packages.${system};
-      cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+      toolchain = fenix.packages.${system}.stable;
     in
     {
-      packages.default = (pkgs.makeRustPlatform {
-        inherit (rust.stable) cargo rustc;
-      }).buildRustPackage {
-        pname = cargoToml.package.name;
-        version = cargoToml.package.version;
-
-        cargoLock.lockFile = ./Cargo.lock;
-
-        src = builtins.filterSource
-          # Exclude `target` because it's huge
-          (path: type: !(type == "directory" && baseNameOf path == "target"))
-          ./.;
-
-        # This is disabled so CI can be impure and not break Nix builds
-        doCheck = false;
+      packages.default = (pkgs.callPackage naersk {
+        inherit (toolchain) rustc cargo;
+      }).buildPackage {
+        src = ./.;
       };
 
-      devShells.default = self.packages.${system}.default.overrideAttrs (old: {
+      devShells.default = pkgs.mkShell {
         # Rust Analyzer needs to be able to find the path to default crate
         # sources, and it can read this environment variable to do so
-        RUST_SRC_PATH = "${rust.stable.rust-src}/lib/rustlib/src/rust/library";
+        RUST_SRC_PATH = "${toolchain.rust-src}/lib/rustlib/src/rust/library";
 
-        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ (with pkgs; [
-          graphviz
+        # Development tools
+        nativeBuildInputs = (with toolchain; [
+          cargo
+          clippy
+          rust-src
+          rustc
+
+          # Always use nightly rustfmt because most of its options are unstable
+          fenix.packages.${system}.latest.rustfmt
+        ]) ++ (with pkgs; [
+          file
+          ncurses
           nixpkgs-fmt
-        ]) ++ (with rust; [
-          latest.rustfmt
-          stable.clippy
-          stable.rust-src
+          shellcheck
+          shfmt
         ]) ++ (with pkgs.nodePackages; [
           markdownlint-cli
         ]);
-      });
+      };
 
       checks = {
         packagesDefault = self.packages.${system}.default;

commit 8f861248367b815561d5beed3487ec382648b217

add graphviz back
I didn't realize the changes in 64f24a6dcfcf11d3447628b3a9b0838d0b933bf3
caused it to go away.

flake.nix
@@ -49,6 +49,7 @@
           fenix.packages.${system}.latest.rustfmt
         ]) ++ (with pkgs; [
           file
+          graphviz
           ncurses
           nixpkgs-fmt
           shellcheck