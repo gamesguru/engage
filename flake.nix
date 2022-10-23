07f590b7b37c807b6a2af5432d408128e00163d7
Charles Hall <charles@computer.surgery>
2022-10-23 01:42:32 -0700

    update flake.nix according to project template

--- flake.nix
+++ flake.nix
@@ -24,30 +24,43 @@
     }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};
+      mkToolchain = fenix.packages.${system}.combine;
+
       toolchain = fenix.packages.${system}.stable;
+
+      buildToolchain = mkToolchain (with toolchain; [
+        cargo
+        rustc
+      ]);
+
+      devToolchain = mkToolchain (with toolchain; [
+        cargo
+        clippy
+        rust-src
+        rustc
+
+        # Always use nightly rustfmt because most of its options are unstable
+        fenix.packages.${system}.latest.rustfmt
+      ]);
     in
     {
       packages.default = (pkgs.callPackage naersk {
-        inherit (toolchain) rustc cargo;
+        cargo = buildToolchain;
+        rustc = buildToolchain;
       }).buildPackage {
         src = ./.;
       };
 
       devShells.default = pkgs.mkShell {
         # Rust Analyzer needs to be able to find the path to default crate
-        # sources, and it can read this environment variable to do so
-        RUST_SRC_PATH = "${toolchain.rust-src}/lib/rustlib/src/rust/library";
+        # sources, and it can read this environment variable to do so. The
+        # `rust-src` component is required in order for this to work.
+        RUST_SRC_PATH = "${devToolchain}/lib/rustlib/src/rust/library";
 
         # Development tools
-        nativeBuildInputs = (with toolchain; [
-          cargo
-          clippy
-          rust-src
-          rustc
-
-          # Always use nightly rustfmt because most of its options are unstable
-          fenix.packages.${system}.latest.rustfmt
-        ]) ++ (with pkgs; [
+        nativeBuildInputs = [
+          devToolchain
+        ] ++ (with pkgs; [
           cargo-insta
           graphviz
           nixpkgs-fmt
