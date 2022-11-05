958fe5d520253c0d4027567efe97590470a8be26
Charles Hall <charles@computer.surgery>
2022-11-05 15:06:54 -0700

    don't `use` self
    This was a copy+paste+LSP error.

--- src/ui.rs
+++ src/ui.rs
@@ -11,7 +11,7 @@ use tokio::{
     process::Command,
 };
 
-use crate::{error, file, ui};
+use crate::{error, file};
 
 /// The separator that appears between the task's name and group and its output
 pub static OUTPUT_SEPARATOR: &str = "│";
@@ -99,7 +99,7 @@ where
                 SetAttribute(Attribute::Reset),
                 Print(format!(
                     "{:>width$} ",
-                    ui::names_to_prefix(&task.group, &task.name),
+                    names_to_prefix(&task.group, &task.name),
                     width = longest_prefix,
                 )),
                 Print(match kind {
