170111d22305643b9298452f53141c7398e3417f
Charles Hall <charles@computer.surgery>
2023-02-05 00:39:07 -0800

    rewrite main with ExitStatus and early returns
    This is the first time I've ever actually used let-else. `rustfmt`
    doesn't have a clue what to do with it, apparently.

--- src/error.rs
+++ src/error.rs
@@ -130,6 +130,16 @@ pub enum Task {
     ExitStatus(ExitStatus),
 }
 
+impl Task {
+    /// Get the inner [`ExitStatus`] if that was the cause
+    pub fn exit_status(&self) -> Option<ExitStatus> {
+        match self {
+            Self::ExitStatus(x) => Some(*x),
+            _ => None,
+        }
+    }
+}
+
 /// The graph could not be created
 #[derive(Debug, Error)]
 pub enum Graph {
