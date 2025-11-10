gamesguru/engage

commit cb470cebb8b2a2e06dad3c2ede8e453a25bfd169

rewrite `graph::execute`
It's a lot simpler now. TaskTracker is much better about managing
resources, in particular, freeing them as early as possible. The
BTreeMap is also replaced with a Vec, to eliminate any risk of errors
getting lost due to equal keys.

Cargo.toml
@@ -116,6 +116,7 @@ petgraph = "0.8.2"
 serde = { version = "1.0.219", features = ["derive"] }
 serde_json = "1.0.142"
 tokio = { version = "1.47.1", features = ["full"] }
+tokio-util = { version = "0.7.16", features = ["rt"] }
 toml = "0.9.5"
 
 [dev-dependencies]

src/graph.rs
@@ -1,23 +1,26 @@
 //! Facilities for working with the graph of groups and tasks.
 
 use std::{
-    collections::{BTreeMap, HashMap},
+    collections::HashMap,
     fmt::Display,
     future::Future,
     ops::ControlFlow,
-    sync::Arc,
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+    },
 };
 
 use petgraph::{
-    Directed, Direction, Graph,
-    algo::{has_path_connecting, tarjan_scc},
-    graph::{DiGraph, IndexType, NodeIndex},
+    Direction,
+    algo::tarjan_scc,
+    graph::{DiGraph, IndexType},
     visit::{
         DfsEvent, Reversed, VisitMap as _, Visitable as _, depth_first_search,
     },
 };
 use serde::{Deserialize, Serialize};
-use tokio::{sync::mpsc, task::JoinSet, time::Instant};
+use tokio_util::task::TaskTracker;
 
 use crate::{
     config::{Config, Group, Task},
@@ -156,144 +159,58 @@ where
 
 /// Run tasks in parallel based on a directed graph.
 ///
-/// This will deadlock if `graph` is not acyclic.
-pub(crate) async fn execute<N, E, Ix, F, Fut, B>(
-    graph: Arc<DiGraph<N, E, Ix>>,
-    task: F,
-) -> BTreeMap<Instant, B>
-where
+/// # Panics
+///
+/// Panics if `graph` has cycles.
+pub(crate) async fn execute<N, E, Ix, F, Fut>(
+    graph: &DiGraph<N, E, Ix>,
+    visit: F,
+) where
     N: Clone + Send + Sync + 'static,
     E: Send + Sync + 'static,
     Ix: IndexType + Send + Sync,
     F: Send + 'static + Fn(N) -> Fut,
-    Fut: Future<Output = ControlFlow<B>> + Send + 'static,
-    B: std::fmt::Debug + Send + Sync + 'static,
+    Fut: Future<Output = ControlFlow<()>> + Send + 'static,
 {
     // If there are no nodes, there is nothing to do.
     if graph.node_count() == 0 {
-        return BTreeMap::new();
+        return;
     }
 
-    let (visit_tx, visit_rx) = mpsc::channel(16);
-    let (ready_tx, ready_rx) = mpsc::channel(16);
-
-    let scheduler = tokio::spawn(scheduler(graph.clone(), visit_rx, ready_tx));
-
-    let scheduler_handle = Arc::new(scheduler.abort_handle());
-
-    let executor = tokio::spawn(executor(
-        graph,
-        task,
-        ready_rx,
-        visit_tx,
-        scheduler_handle,
-    ));
-
-    scheduler.await.expect("should be able to join scheduler");
-    executor.await.expect("should be able to join executor")
-}
-
-/// Consumes visited nodes and produces readied nodes based on the graph.
-async fn scheduler<N, E, Ix>(
-    graph: Arc<Graph<N, E, Directed, Ix>>,
-    mut visit_rx: mpsc::Receiver<(NodeIndex<Ix>, bool)>,
-    ready_tx: mpsc::Sender<NodeIndex<Ix>>,
-) where
-    Ix: IndexType,
-{
     let mut visit_map = graph.visit_map();
+    let mut layer = graph.externals(Direction::Incoming).collect::<Vec<_>>();
 
-    for entrypoint in graph.externals(Direction::Incoming) {
-        ready_tx.send(entrypoint).await.expect("channel should still be open");
-    }
+    while !layer.is_empty() {
+        let abort = Arc::new(AtomicBool::new(false));
+        let task_tracker = TaskTracker::new();
 
-    while let Some((visited, ok)) = visit_rx.recv().await {
-        visit_map.visit(visited);
-
-        let all_nodes_visited =
-            graph.node_indices().all(|node| visit_map.is_visited(&node));
-
-        if all_nodes_visited || !ok {
-            // We're done!
-            return;
-        }
-
-        let ready_nodes = graph
-            .node_indices()
-            .filter(|node| {
-                // We only care about nodes connected to this visited node.
-                has_path_connecting(graph.as_ref(), visited, *node, None)
-            })
-            .filter(|node| {
-                // We only care about this node's dependency.
-                graph
-                    .neighbors_directed(*node, Direction::Incoming)
-                    .all(|node| visit_map.is_visited(&node))
-            })
-            .filter(|node| {
-                // We don't want to revisit nodes.
-                !visit_map.is_visited(node)
+        for &node in &layer {
+            let visit = visit(graph[node].clone());
+            assert!(visit_map.visit(node), "cycle detected");
+            let abort = abort.clone();
+            task_tracker.spawn(async move {
+                if visit.await.is_break() {
+                    abort.store(true, Ordering::SeqCst);
+                }
             });
-
-        for node in ready_nodes {
-            ready_tx.send(node).await.expect("channel should still be open");
         }
-    }
-}
 
-/// Consumes and executes readied nodes and produces visited nodes.
-async fn executor<N, E, Ix, F, Fut, B>(
-    graph: Arc<Graph<N, E, Directed, Ix>>,
-    task: F,
-    mut ready_rx: mpsc::Receiver<NodeIndex<Ix>>,
-    visit_tx: mpsc::Sender<(NodeIndex<Ix>, bool)>,
-    scheduler_handle: Arc<tokio::task::AbortHandle>,
-) -> BTreeMap<Instant, B>
-where
-    N: Clone,
-    Ix: IndexType + Send,
-    F: Fn(N) -> Fut,
-    Fut: Future<Output = ControlFlow<B>> + Send + 'static,
-    B: std::fmt::Debug + Send + Sync + 'static,
-{
-    let mut join_set = JoinSet::new();
+        task_tracker.close();
+        task_tracker.wait().await;
 
-    while let Some(node) = ready_rx.recv().await {
-        let task = task(graph[node].clone());
-
-        let visit_tx = visit_tx.clone();
-        let scheduler_handle = scheduler_handle.clone();
-        join_set.spawn(async move {
-            let control_flow = task.await;
-
-            let result =
-                visit_tx.send((node, control_flow.is_continue())).await;
-
-            // `is_finished()` has a pretty big caveat so I wouldn't be too
-            // surprised if this results in spurious panics. Hopefully this
-            // works how I want, and worst-case we can just always ignore
-            // a send error, which *should* be fine.
-            if !scheduler_handle.is_finished() {
-                // This is only a problem if the scheduler is still alive.
-                result.expect("channel should still be open");
-            }
-
-            (control_flow, Instant::now())
-        });
-    }
-
-    let mut results = BTreeMap::new();
-
-    // Join all tasks.
-    while let Some(result) = join_set.join_next().await {
-        if let (ControlFlow::Break(x), exit_instant) =
-            result.expect("should be able to join task")
-        {
-            results.insert(exit_instant, x);
+        if abort.load(Ordering::SeqCst) {
+            break;
         }
-    }
 
-    results
+        // Discover the next layer of nodes, discarding duplicate nodes due to
+        // having multiple incoming edges from the previous layer.
+        let mut visit_map = graph.visit_map();
+        layer = layer
+            .into_iter()
+            .flat_map(|x| graph.neighbors(x))
+            .filter(|&x| visit_map.visit(x))
+            .collect();
+    }
 }
 
 /// Build a graph of the groups and tasks to be executed.

src/ui.rs
@@ -10,7 +10,7 @@ use petgraph::graph::{DiGraph, IndexType};
 use tokio::{
     io::{AsyncBufReadExt as _, AsyncRead, BufReader},
     process::Command,
-    sync::Semaphore,
+    sync::{Semaphore, mpsc},
 };
 
 use crate::{
@@ -228,9 +228,21 @@ where
         "starting".blue().bold(),
     );
 
-    let errors = graph::execute(Arc::new(graph), move |node| {
+    let (error_tx, mut error_rx) = mpsc::channel(16);
+    let error_collector = tokio::spawn(async move {
+        let mut errors = Vec::new();
+
+        while let Some(next) = error_rx.recv().await {
+            errors.push(next);
+        }
+
+        errors
+    });
+
+    graph::execute(&graph, move |node| {
         let config = config.clone();
         let semaphore = semaphore.clone();
+        let error_tx = error_tx.clone();
         async move {
             let permit = if let Some(semaphore) = semaphore {
                 Some(
@@ -248,7 +260,12 @@ where
                 if let Err(e) =
                     run_task(&config, longest_prefix, task.clone()).await
                 {
-                    return ControlFlow::Break((task, e));
+                    error_tx
+                        .send((task, e))
+                        .await
+                        .expect("channel should still be open");
+
+                    return ControlFlow::Break(());
                 }
             }
 
@@ -259,8 +276,10 @@ where
     })
     .await;
 
-    let errors = errors
-        .into_values()
+    let errors = error_collector
+        .await
+        .expect("should be able to join task")
+        .into_iter()
         .map(|(task, error)| error::TaskContext {
             task: task.name.clone(),
             group: task.group.clone(),

commit 3a23bee5ad24ac7430c31c7185b49aeb5411ff15

rewrite `graph::execute` again
This fixes a bug where given a graph of nodes A, B, and C with an edge
from A to B, where A succeeds immediately and C sleeps forever, B is
never run despite its dependencies being satisfied. A test for this case
is now included.

The bug was introduced in cb470cebb8b2a2e06dad3c2ede8e453a25bfd169
and the fix is to more or less go back to how it was before. This
implementation is still an improvement though, particularly in how new
nodes to visit are calculated.

Cargo.toml
@@ -110,12 +110,16 @@ clap_complete = "4.5.58"
 crossterm = "0.29.0"
 derail = "0.3.0"
 derail-macros = "0.3.0"
+either = "1.15.0"
+futures-concurrency = "7.6.3"
+futures-util = "0.3.31"
 indoc = "2.0.6"
 itertools = "0.14.0"
 petgraph = "0.8.3"
 serde = { version = "1.0.228", features = ["derive"] }
 serde_json = "1.0.145"
 tokio = { version = "1.47.1", features = ["full"] }
+tokio-stream = "0.1.17"
 tokio-util = { version = "0.7.16", features = ["rt"] }
 toml = "0.9.7"
 

src/graph.rs
@@ -5,12 +5,11 @@ use std::{
     fmt,
     future::Future,
     ops::ControlFlow,
-    sync::{
-        Arc,
-        atomic::{AtomicBool, Ordering},
-    },
 };
 
+use either::Either::{Left, Right};
+use futures_concurrency::future::FutureExt as _;
+use futures_util::FutureExt as _;
 use petgraph::{
     Direction,
     algo::tarjan_scc,
@@ -19,7 +18,12 @@ use petgraph::{
         DfsEvent, Reversed, VisitMap as _, Visitable as _, depth_first_search,
     },
 };
-use tokio_util::task::TaskTracker;
+use tokio::sync::mpsc;
+use tokio_stream::{
+    StreamExt as _,
+    wrappers::{ReceiverStream, UnboundedReceiverStream},
+};
+use tokio_util::{sync::CancellationToken, task::TaskTracker};
 
 use crate::{
     config::Task,
@@ -134,11 +138,8 @@ where
     Ok(subgraph)
 }
 
-/// Run tasks in parallel based on a directed graph.
-///
-/// # Panics
-///
-/// Panics if `graph` has cycles.
+/// Run `visit` for each node in `graph` in parallel, ordered by `graph`'s
+/// edges.
 pub(crate) async fn execute<N, E, Ix, F, Fut>(
     graph: &DiGraph<N, E, Ix>,
     visit: F,
@@ -149,45 +150,75 @@ pub(crate) async fn execute<N, E, Ix, F, Fut>(
     F: Send + 'static + Fn(N) -> Fut,
     Fut: Future<Output = ControlFlow<()>> + Send + 'static,
 {
+    let mut visit_map = graph.visit_map();
+
     // If there are no nodes, there is nothing to do.
-    if graph.node_count() == 0 {
+    if visit_map.is_full() {
         return;
     }
 
-    let mut visit_map = graph.visit_map();
-    let mut layer = graph.externals(Direction::Incoming).collect::<Vec<_>>();
+    let loop_ct = CancellationToken::new();
+    let task_tracker = TaskTracker::new();
+    let (visited_tx, visited_rx) = mpsc::channel(1);
 
-    while !layer.is_empty() {
-        let abort = Arc::new(AtomicBool::new(false));
-        let task_tracker = TaskTracker::new();
+    // Use an unbounded channel to avoid deadlocks because:
+    //
+    // * Multiple indices can become ready to visit at once.
+    // * The producer and consumer are multiplexed on a single task.
+    let (visit_tx, visit_rx) = mpsc::unbounded_channel();
 
-        for &node in &layer {
-            let visit = visit(graph[node].clone());
-            assert!(visit_map.visit(node), "cycle detected");
-            let abort = abort.clone();
-            task_tracker.spawn(async move {
-                if visit.await.is_break() {
-                    abort.store(true, Ordering::SeqCst);
+    let mut ixes = tokio_stream::iter(graph.externals(Direction::Incoming))
+        .chain(UnboundedReceiverStream::new(visit_rx))
+        .map(Left)
+        .merge(ReceiverStream::new(visited_rx).map(Right));
+
+    while let Some(ix) =
+        ixes.next().race(loop_ct.cancelled().map(|()| None)).await
+    {
+        match ix {
+            // Visit an index.
+            Left(ix) => {
+                let visit = visit(graph[ix].clone());
+                let loop_ct = loop_ct.clone();
+                let visited_tx = visited_tx.clone();
+
+                task_tracker.spawn(async move {
+                    if visit.await.is_break() {
+                        loop_ct.cancel();
+                    } else {
+                        visited_tx
+                            .send(ix)
+                            .await
+                            .expect("channel should be open");
+                    }
+                });
+            }
+
+            // Compute new indices to visit.
+            Right(ix) => {
+                visit_map.visit(ix);
+
+                if visit_map.is_full() {
+                    break;
                 }
-            });
+
+                let ixes = graph
+                    .neighbors_directed(ix, Direction::Outgoing)
+                    .filter(|&ix| {
+                        graph
+                            .neighbors_directed(ix, Direction::Incoming)
+                            .all(|ix| visit_map.contains(ix.index()))
+                    });
+
+                for ix in ixes {
+                    visit_tx.send(ix).expect("channel should be open");
+                }
+            }
         }
-
-        task_tracker.close();
-        task_tracker.wait().await;
-
-        if abort.load(Ordering::SeqCst) {
-            break;
-        }
-
-        // Discover the next layer of nodes, discarding duplicate nodes due to
-        // having multiple incoming edges from the previous layer.
-        let mut visit_map = graph.visit_map();
-        layer = layer
-            .into_iter()
-            .flat_map(|x| graph.neighbors(x))
-            .filter(|&x| visit_map.visit(x))
-            .collect();
     }
+
+    task_tracker.close();
+    task_tracker.wait().await;
 }
 
 /// Build a graph of the tasks to be executed.

tests/integrations/fixtures/a_then_b_and_c.toml
@@ -1,0 +1,9 @@
+[tasks.a]
+command = ["sleep", "infinity"]
+
+[tasks.b]
+command = ["true"]
+
+[tasks.c]
+command = ["echo", "pass"]
+after = ["b"]

tests/integrations/main.rs
@@ -5,13 +5,15 @@
 
 use std::{
     fs,
-    process::{Command, Output},
+    process::{Command, Output, Stdio},
+    time::Duration,
 };
 
-use assert_cmd::cargo::CommandCargoExt as _;
+use assert_cmd::cargo::{CommandCargoExt as _, cargo_bin};
 use path_macro::path;
 use strip_ansi_escapes::strip;
 use tempfile::tempdir;
+use tokio::{io::AsyncReadExt as _, time::timeout};
 
 type TestError = Box<dyn std::error::Error>;
 type TestResult = Result<(), TestError>;
@@ -368,6 +370,45 @@ fn report_all_errors() -> TestResult {
         set_snapshot_suffix!("status_code");
         insta::assert_debug_snapshot!(status_code);
     });
+
+    Ok(())
+}
+
+// Tests that tasks are executed as soon as their dependencies are ready, rather
+// than being blocked on other tasks they don't have an explicit (direct or
+// transitive) dependency on.
+#[tokio::test]
+async fn a_then_b_and_c() -> TestResult {
+    let mut child = tokio::process::Command::new(cargo_bin!("engage"))
+        .args(["--file", "tests/integrations/fixtures/a_then_b_and_c.toml"])
+        .stdout(Stdio::piped())
+        .spawn()?;
+
+    let mut stdout = child.stdout.take().expect("stdout should be set");
+
+    let find_needle_in_haystack = async {
+        let needle = b"pass";
+        let mut haystack = Vec::new();
+        let mut buf = [0; 32];
+
+        loop {
+            let n = stdout.read(&mut buf).await?;
+
+            haystack.extend_from_slice(&buf[..n]);
+
+            if haystack.windows(needle.len()).any(|x| x == needle) {
+                break;
+            }
+        }
+
+        Ok::<_, TestError>(())
+    };
+
+    let res = timeout(Duration::from_secs(1), find_needle_in_haystack).await;
+
+    child.kill().await?;
+
+    res??;
 
     Ok(())
 }